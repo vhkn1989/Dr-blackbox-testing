280aa5d6e3a489a4c3a8d9df76fc847e812d8a67
Veerepalli Harikrishna -X (vharikri - WIPRO LIMITED at Cisco) <vharikri@cisco.com>
2022-08-27 07:52:07 +0530

    added number data type file

---
+++ 1_number.py
@@ -0,0 +1,32 @@
+'''
+1. Number data type is used to store integer values(0-9).
+2. Number is a immutable object
+    Example :-  N = 123
+
+    1. 'N' is a Variable
+    2. '=' is a assignment operator
+    3. '123' is a value
+    4. 'N' Data type is number
+
+3. type() function is used to know type of object.
+'''
+
+# To Create a variable with Number data type
+n = 123
+print(n)
+# print('memory allocation for n variable is:', id(n))
+#To Update and Append
+# h = 123
+# print(h)
+# print('memory location for n variable is:',id(h))
+# To delete a Variable
+# del n
+# print(n)
+h = 56.23
+print(h)
+print(type(h))
+# To Know variable Data type
+print(type(n))
+
+
+
