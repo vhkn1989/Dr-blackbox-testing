add3e7dbaac9f53624848aaa724c8f2718495522
Veerepalli Harikrishna -X (vharikri - WIPRO LIMITED at Cisco) <vharikri@cisco.com>
2022-08-28 07:09:59 +0530

    Deleted complex number file

--- Complex_Num.py
+++
@@ -1,8 +0,0 @@
-import cmath
-x = 4.5
-y = 9
-z = complex(x,y);
-print("The real part of complex number is:",end="")
-print(z.real)
-print("The imaginary part of complex number is:",end="")
-print(z.imag)
